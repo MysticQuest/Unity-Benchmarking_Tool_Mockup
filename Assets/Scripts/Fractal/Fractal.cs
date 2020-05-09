1bfd68813deeff3c98e14dd4e77a93146c2d414b
Mystic Quest <l1skou@gmail.com>
2020-05-10 02:41:10 +0300

    fractals init

---
+++ Assets/Scripts/Fractal/Fractal.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour
+{
+    public Mesh mesh;
+    public Material material;
+
+    public int maxDepth;
+    private int depth;
+
+    private void Start()
+    {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+        if (depth < maxDepth)
+        {
+            new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this);
+        }
+    }
+
+    private void Initialize(Fractal parent)
+    {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        transform.parent = parent.transform;
+    }
+}
