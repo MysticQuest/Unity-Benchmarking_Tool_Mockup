ecbaffdd2fe2e1ed10f895404c5e663df7f827d5
Mystic Quest <l1skou@gmail.com>
2020-05-09 23:28:43 +0300

    some refactoring

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Graph : MonoBehaviour
 {
-    [Range(0, 1)]
-    public int function;
+    public GraphFunctionName function;
+    [Range(10, 300)]
+    public int resolution = 10;
+
+    static GraphFunction[] functions = {
+        SineFunction, MultiSineFunction
+    };
 
     [SerializeField] private Transform pointPrefab;
-    [Range(50, 300)]
-    public int resolution = 10;
     Transform[] points;
 
     void Awake()
@@ -23,7 +26,6 @@ public class Graph : MonoBehaviour
         {
             Transform point = Instantiate(pointPrefab);
             position.x = (i + 0.5f) * step - 1f;
-            // position.y = position.x * position.x * position.x;
             position.y = 0;
             point.localPosition = position;
             point.localScale = scale;
@@ -35,28 +37,22 @@ public class Graph : MonoBehaviour
     private void Update()
     {
         float t = Time.time;
+        GraphFunction runFunc = functions[(int)function];
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            if (function == 0)
-            {
-                position.y = SineFunction(position.x, t);
-            }
-            else
-            {
-                position.y = MultiSineFunction(position.x, t);
-            }
+            position.y = runFunc(position.x, t);
             point.localPosition = position;
         }
     }
 
-    float SineFunction(float x, float t)
+    static float SineFunction(float x, float t)
     {
         return Mathf.Sin(Mathf.PI * (x + t));
     }
 
-    float MultiSineFunction(float x, float t)
+    static float MultiSineFunction(float x, float t)
     {
         float y = Mathf.Sin(Mathf.PI * (x + t));
         y += Mathf.Sin(2f * Mathf.PI * (x + 2f * t)) / 2f;
