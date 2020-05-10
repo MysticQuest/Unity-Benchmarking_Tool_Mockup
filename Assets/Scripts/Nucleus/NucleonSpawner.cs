811c10bb1802df103654b0c00c65311197241492
Mystic Quest <l1skou@gmail.com>
2020-05-10 10:01:32 +0300

    fps counter

--- Assets/Scripts/Nucleus/NucleonSpawner.cs
+++ Assets/Scripts/Nucleus/NucleonSpawner.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class NucleonSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float timeBetweenSpawns;
+    public float spawnDistance;
+    public Nucleon[] nucleonPrefabs;
+
+    float timeSinceLastSpawn;
+
+    void FixedUpdate()
     {
-        
+        timeSinceLastSpawn += Time.deltaTime;
+        if (timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnNucleon()
     {
-        
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
     }
 }
