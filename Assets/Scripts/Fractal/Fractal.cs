c149ee6cdbe5e133673d04ec899a89ebed4b057f
Mystic Quest <l1skou@gmail.com>
2020-05-10 08:15:50 +0300

    fractal gen, colors

--- Assets/Scripts/Fractal/Fractal.cs
+++ Assets/Scripts/Fractal/Fractal.cs
@@ -8,28 +8,94 @@ public class Fractal : MonoBehaviour
     public Material material;
 
     public int maxDepth;
-    public float childScale;
     private int depth;
+    public float childScale;
+
+    private Material[,] materials;
+
+    private static Vector3[] childDirections = {
+        Vector3.up,
+        Vector3.right,
+        Vector3.left,
+        Vector3.forward,
+        Vector3.back,
+        // Vector3.down  //root downwards
+    };
+
+    private static Quaternion[] childOrientations = {
+        Quaternion.identity,
+        Quaternion.Euler(0f, 0f, -90f),
+        Quaternion.Euler(0f, 0f, 90f),
+        Quaternion.Euler(90f, 0f, 0f),
+        Quaternion.Euler(-90f, 0f, 0f),
+        // Quaternion.Euler(180f, 0f, 0f)  //root downwards
+    };
 
     private void Start()
     {
+        if (materials == null)
+        {
+            InitializeMaterials();
+        }
         gameObject.AddComponent<MeshFilter>().mesh = mesh;
-        gameObject.AddComponent<MeshRenderer>().material = material;
+        gameObject.AddComponent<MeshRenderer>().material = materials[depth, Random.Range(0, 2)];
         if (depth < maxDepth)
         {
-            new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this);
+            StartCoroutine(CreateChildren());
         }
     }
 
-    private void Initialize(Fractal parent)
+    private IEnumerator CreateChildren()
+    {
+        // if (depth == 0)  //root downwards
+        {
+            for (int i = 0; i < childDirections.Length; i++)
+            {
+                yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+                new GameObject("Fractal Child").AddComponent<Fractal>().
+                    Initialize(this, i);
+            }
+        }
+        // else  //root downwards
+        // {
+        //     for (int i = 0; i < childDirections.Length - 1; i++)
+        //     {
+        //         yield return new WaitForSeconds(0.5f);
+        //         new GameObject("Fractal Child").AddComponent<Fractal>().
+        //             Initialize(this, i);
+        //     }
+        // }
+    }
+
+    private void InitializeMaterials()
+    {
+        materials = new Material[maxDepth + 1, 2];
+        for (int i = 0; i <= maxDepth; i++)
+        {
+            float t = i / (maxDepth - 1f);
+            t *= t;
+            materials[i, 0] = new Material(material);
+            materials[i, 0].color = Color.Lerp(Color.white, Color.yellow, t);
+            materials[i, 1] = new Material(material);
+            materials[i, 1].color = Color.Lerp(Color.white, Color.cyan, t);
+        }
+        materials[maxDepth, 0].color = Color.magenta;
+        materials[maxDepth, 1].color = Color.red;
+    }
+
+    private void Initialize(Fractal parent, int childIndex)
     {
         mesh = parent.mesh;
-        material = parent.material;
+        materials = parent.materials;
         maxDepth = parent.maxDepth;
         depth = parent.depth + 1;
         childScale = parent.childScale;
         transform.parent = parent.transform;
         transform.localScale = Vector3.one * childScale;
-        transform.localPosition = Vector3.up * (0.5f + 0.5f * childScale);
+        transform.localPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale);
+        transform.localRotation = childOrientations[childIndex];
     }
+
+
+
 }
